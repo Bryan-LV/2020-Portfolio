be1d280248701a34fafd8684d7ac41a4827f12a1
Bryan Almaraz <bryan.almaraz96@gmail.com>
2020-02-26 12:19:25 +0800

    edit bio

--- src/components/layout/Info.js
+++ src/components/layout/Info.js
@@ -5,8 +5,8 @@ export default function Info(props) {
     <section className="section--dark" id="about">
       <section className="section section--text-white">
         <h1 className="section__title section__title--big section__text--green section__title--underline">What I do</h1>
-        <h3 className="section__subtitle">Full-Stack Developer Based In Berlin</h3>
-        <p className="section__paragraph section__paragraph--light info__paragraph">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore, commodi. Iste magni adipisci perspiciatis natus, illum at dicta maxime nesciunt nulla dolores minus labore qui dolorum delectus aliquid blanditiis quisquam aut ab repellendus dolorem reprehenderit illo, iusto cum. Commodi temporibus hic neque dolore provident totam quos eius quidem assumenda. Cum quos quasi earum dolores alias optio repellat, voluptates enim sunt.</p>
+        <h3 className="section__subtitle">Full-Stack JS Developer</h3>
+        <p className="section__paragraph section__paragraph--light info__paragraph">My primary focus is in front-end applications using React and vanilla javascript. I am well versed in the land of CSS, being able to work with pre-proccessors and CSS-in-JS libraries. I have helped many business owners create elegant and responsive designs with good UX/UI principles. When I am not working, you can find me exploring my other passions in music, and exploring the world.</p>
       </section>
     </section>
   )
